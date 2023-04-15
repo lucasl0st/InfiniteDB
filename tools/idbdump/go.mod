6f6d1937e1f11ec75d7e2c5ae963cd2031d19887
Lucas Pape <lucaspape2002@protonmail.com>
2023-04-15 22:23:01 +0200

    chore: add build structure for tools

---
+++ tools/idbdump/go.mod
@@ -0,0 +1,3 @@
+module github.com/lucasl0st/InfiniteDB/tools/idbdump
+
+go 1.20
---
+++ tools/idbdump/main.go
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023 Lucas Pape
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
