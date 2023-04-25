22f72db3235633ed3d88496b64202b022ed4abb1
Lucas <lpape@proton.me>
2023-04-25 19:02:10 +0200

    fix: generate auth key if not exists (#44)

--- server/internal.go
+++ server/internal.go
@@ -23,89 +23,6 @@ const AuthenticationTableFieldKeyValue = "keyValue"
 
 const AuthenticationKeyMain = "main"
 
-func SetupAuthenticationTable(idb *idblib.IDB) error {
-	r, err := idb.GetDatabase(InternalDatabase)
-
-	if err != nil {
-		return err
-	}
-
-	for _, t := range r.Tables {
-		if t == AuthenticationTable {
-			return nil
-		}
-	}
-
-	fields := make(map[string]field.Field)
-	fields[AuthenticationTableFieldKeyId] = field.Field{
-		Name:    AuthenticationTableFieldKeyId,
-		Indexed: true,
-		Unique:  true,
-		Type:    dbtype.TEXT,
-	}
-
-	fields[AuthenticationTableFieldKeyValue] = field.Field{
-		Name:    AuthenticationTableFieldKeyValue,
-		Indexed: true,
-		Unique:  false,
-		Type:    dbtype.TEXT,
-	}
-
-	_, err = idb.CreateTableInDatabase(InternalDatabase, AuthenticationTable, fields, request.TableOptions{})
-
-	if err != nil {
-		return err
-	}
-
-	mainKey := uuid.New().String()
-
-	mainKeyObject := make(map[string]json.RawMessage)
-	mainKeyObject[AuthenticationTableFieldKeyId] = util.StringToJsonRaw(AuthenticationKeyMain)
-	mainKeyObject[AuthenticationTableFieldKeyValue] = util.StringToJsonRaw(mainKey)
-
-	_, err = idb.InsertToDatabaseTable(InternalDatabase, AuthenticationTable, mainKeyObject)
-
-	if err != nil {
-		return err
-	}
-
-	l.Println("generated main authentication key (will only be shown once): " + mainKey)
-
-	return nil
-}
-
-func authenticated(idb *idblib.IDB, key string) (bool, error) {
-	r := table.Request{
-		Query: &table.Query{
-			Where: &request.Where{
-				Field:    AuthenticationTableFieldKeyValue,
-				Operator: request.EQUALS,
-				Value:    util.StringToJsonRaw(key),
-			},
-		},
-	}
-
-	res, err := idb.GetFromDatabaseTable(InternalDatabase, AuthenticationTable, r)
-
-	if err != nil {
-		return false, err
-	}
-
-	for _, result := range res.Results {
-		s, err := util.JsonRawToString(result[AuthenticationTableFieldKeyValue])
-
-		if err != nil {
-			return false, err
-		}
-
-		if *s == key {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
-
 func SetupInternalDatabase(idb *idblib.IDB) error {
 	r, err := idb.GetDatabases()
 
@@ -126,4 +43,106 @@ func SetupInternalDatabase(idb *idblib.IDB) error {
 	}
 
 	return nil
+}
+
+func SetupAuthenticationTable(idb *idblib.IDB) error {
+	r, err := idb.GetDatabase(InternalDatabase)
+
+	if err != nil {
+		return err
+	}
+
+	createTable := true
+
+	for _, t := range r.Tables {
+		if t == AuthenticationTable {
+			createTable = false
+			break
+		}
+	}
+
+	if createTable {
+		fields := make(map[string]field.Field)
+		fields[AuthenticationTableFieldKeyId] = field.Field{
+			Name:    AuthenticationTableFieldKeyId,
+			Indexed: true,
+			Unique:  true,
+			Type:    dbtype.TEXT,
+		}
+
+		fields[AuthenticationTableFieldKeyValue] = field.Field{
+			Name:    AuthenticationTableFieldKeyValue,
+			Indexed: true,
+			Unique:  false,
+			Type:    dbtype.TEXT,
+		}
+
+		_, err = idb.CreateTableInDatabase(InternalDatabase, AuthenticationTable, fields, request.TableOptions{})
+
+		if err != nil {
+			return err
+		}
+	}
+
+	res, err := idb.GetFromDatabaseTable(InternalDatabase, AuthenticationTable, table.Request{
+		Query: &table.Query{
+			Where: &request.Where{
+				Field:    AuthenticationTableFieldKeyId,
+				Operator: request.EQUALS,
+				Value:    util.StringToJsonRaw(AuthenticationKeyMain),
+			},
+		},
+	})
+
+	if err != nil {
+		return err
+	}
+
+	if len(res.Results) == 0 {
+		mainKey := uuid.New().String()
+
+		mainKeyObject := make(map[string]json.RawMessage)
+		mainKeyObject[AuthenticationTableFieldKeyId] = util.StringToJsonRaw(AuthenticationKeyMain)
+		mainKeyObject[AuthenticationTableFieldKeyValue] = util.StringToJsonRaw(mainKey)
+
+		_, err = idb.InsertToDatabaseTable(InternalDatabase, AuthenticationTable, mainKeyObject)
+
+		if err != nil {
+			return err
+		}
+
+		l.Println("generated main authentication key (will only be shown once): " + mainKey)
+	}
+
+	return nil
+}
+
+func authenticated(idb *idblib.IDB, key string) (bool, error) {
+	res, err := idb.GetFromDatabaseTable(InternalDatabase, AuthenticationTable, table.Request{
+		Query: &table.Query{
+			Where: &request.Where{
+				Field:    AuthenticationTableFieldKeyValue,
+				Operator: request.EQUALS,
+				Value:    util.StringToJsonRaw(key),
+			},
+		},
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	for _, result := range res.Results {
+		s, err := util.JsonRawToString(result[AuthenticationTableFieldKeyValue])
+
+		if err != nil {
+			return false, err
+		}
+
+		if *s == key {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
